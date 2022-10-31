c6ed1133204dfbeb60542c3364d1c7493ca86343
陳建佑 <jianyou224@gmail.com>
2022-10-31 21:02:15 +0800

    'vuex'

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import router from './router';
+import store from './store';
 
 const emitter = mitt();
 
-const app = createApp(App);
+const app = createApp(App).use(store);
 app.config.globalProperties.$emitter = emitter;
 app.use(router);
 app.mount('#app');
